5d1ee301a84767af0a87a4ea2e9613f25944c82d
raneem <eng.raneem.yad@gmail.com>
2024-08-27 13:34:15 +0200

    adding inkwell and gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meal_prep_training/categories_screen.dart';
+import 'package:meal_prep_training/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
---
+++ lib/screens/categories.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:meal_prep_training/data/dummy_data.dart';
+import 'package:meal_prep_training/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick your Category!"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          childAspectRatio: 3 / 2,
+        ),
+        children: availableCategories
+            .map((item) => CategoryGridItem(category: item))
+            .toList(),
+      ),
+    );
+  }
+}
